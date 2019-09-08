8bfaab8786d78639192b6d7017fc1fd993dadc11
Luis Alfredo Del Castillo Rivera <luis.dcr25@gmail.com>
2019-09-07 20:19:49 -0500

    app

--- app/src/main/java/com/example/cafeteriaapp/Adapter/AdapterProductos.java
+++ app/src/main/java/com/example/cafeteriaapp/Adapter/AdapterProductos.java
@@ -1,10 +1,14 @@
 package com.example.cafeteriaapp.Adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -40,6 +44,12 @@ public class AdapterProductos extends RecyclerView.Adapter<AdapterProductos.MyVi
         holder.producto.setText(productoList.get(position).getProducto());
         holder.descripcion.setText(productoList.get(position).getDescripcion());
         holder.precio.setText("$"+ productoList.get(position).getPrecio());
+        String img;
+        img = productoList.get(position).getFoto();
+        byte [] encodeByte= Base64.decode(img,Base64.DEFAULT);
+        Bitmap bitmap= BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+        holder.imgProducto.setImageBitmap(bitmap);
+
 
 
     }
@@ -53,12 +63,14 @@ public class AdapterProductos extends RecyclerView.Adapter<AdapterProductos.MyVi
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
         TextView producto,descripcion,precio;
+        ImageView imgProducto;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             producto = itemView.findViewById(R.id.txtNomProducto);
             descripcion = itemView.findViewById(R.id.txtDescripcion);
             precio = itemView.findViewById(R.id.txtPrecio);
+            imgProducto = itemView.findViewById(R.id.imgProducto);
 
 
         }
--- app/src/main/java/com/example/cafeteriaapp/Entidades/Producto.java
+++ app/src/main/java/com/example/cafeteriaapp/Entidades/Producto.java
@@ -1,5 +1,9 @@
 package com.example.cafeteriaapp.Entidades;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
 import com.google.gson.annotations.SerializedName;
 
 public class Producto {
@@ -9,6 +13,7 @@ public class Producto {
     @SerializedName("Stock") private String Stock;
     @SerializedName("Precio") private String Precio;
     @SerializedName("Foto") private String Foto;
+    @SerializedName("imagen") private Bitmap imagen;
 
     public int getId() {
         return Id;
@@ -32,5 +37,43 @@ public class Producto {
 
     public String getFoto() {
         return Foto;
+    }
+
+    public Bitmap getImagen() {
+        return imagen;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public void setProducto(String producto) {
+        Producto = producto;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+
+    public void setStock(String stock) {
+        Stock = stock;
+    }
+
+    public void setPrecio(String precio) {
+        Precio = precio;
+    }
+
+    public void setFoto(String foto) {
+        Foto = foto;
+        try{
+            byte[] byteCode = Base64.decode(Foto,Base64.DEFAULT);
+            this.imagen = BitmapFactory.decodeByteArray(byteCode,0,byteCode.length);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void setImagen(Bitmap imagen) {
+        this.imagen = imagen;
     }
 }
